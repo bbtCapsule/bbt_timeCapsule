8fe43de3249d737f1a2afae93929e53ac3922548
RA-DM <61676742+RA-DM@users.noreply.github.com>
2020-05-12 09:44:14 +0800

    Update database.py

--- py/api/database/database.py
+++ py/api/database/database.py
@@ -1,6 +1,7 @@
 import pymysql
 from config import db
 import json
+import ahocorasick
 
 class database:
     def getCursor():
@@ -73,5 +74,22 @@ class database:
         con.commit()
         con.close()
 
-
+#对胶囊文本敏感词过滤
+    def capsuleTextCheck(message):
+        check=ahocorasick.Automaton()
+        orignWds=[]
+        aimWds=[]
+        with open('unSeenWord.txt',encoding='utf-8') as f:
+            for line in f.readlines():
+                line=line.strip()
+                orignWds.append(str(line))
+            for index,word in enumerate(orignWds):
+                check.add_word(word,(index,word))
+        check.make_automaton()
+        for i in check.iter(message):
+            wd=i[1][1]
+            aimWds.append(wd)
+        for x in aimWds:
+            text=message.replace(x,'*'*len(x))
+        return text
 
