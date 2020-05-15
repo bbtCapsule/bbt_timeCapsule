48ecc4fe3c09107941cad6aa4f4afd5a1ba7e9ca
Hikoyan <huanghongyan324@gmail.com>
2020-05-16 00:55:28 +0800

    完成挖铲子动画，还差规则介绍页，引导页，和后台对接登录

--- js/show.js
+++ js/show.js
@@ -1,1 +1,37 @@
 //可以把不涉及数据传输的 动画函数、展示写在这里
+function areaShow() {
+    for(i=1;i<8;i++){
+        $("#A"+i).on('click', function (e) {
+            e.preventDefault();
+            var clickId=$(this).attr("id");
+            $("#"+clickId).attr("class","go");
+
+            $("#mai_anim").css("top",e.pageY);
+            $("#mai_anim").css("left",e.pageX);
+            setTimeout(() => {
+                $("#mai_anim").fadeIn(100);
+            }, 2000);
+            //addChanzi();
+            setTimeout(() => {
+                $("#"+clickId).attr("class","");
+                $("#mai_anim").fadeOut(300);
+            }, 3800);
+
+        });
+    }
+
+}
+areaShow();
+$("#mai_att").on('click', function (e) {
+    $("#mai_att").fadeOut(400);
+    areaShow();
+})
+// function addChanzi(){
+//     //出现铲子动画
+// $(document).mousemove(function (e) {
+//     // var xy_keleyi_com="x坐标:"+ e.pageX+",y坐标："+ e.pageY;
+//     // console.log(xy_keleyi_com);
+//     $("#mai_anim").css("top",e.pageY);
+//     $("#mai_anim").css("left",e.pageX);
+//     })
+// }
