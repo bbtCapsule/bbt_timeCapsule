adbf32d4e3e70c50cd220fec06020e39d68ed6f8
kimivictory <2382724968@qq.com>
2020-05-15 10:54:51 +0800

    del

--- js/welcome.js
+++
@@ -1,8 +0,0 @@
-// 第一步：用户同意授权，获取code
-window.location.href = 'https://open.weixin.qq.com/connect/oauth2/authorize?'
-'appid=' + APPID + '&'
-'redirect_uri=' + REDIRECT_URI + '&'
-'response_type=' + RESPONSE_TYPE + '&'
-'scope=' + SCOPE + '&'
-'state=' + STATE + '#wechat_redirect';
-
