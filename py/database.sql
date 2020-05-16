44cbc8f7ee471a061b3774535c41e3aa6c69a2d0
isLXa <59189855+isLXa@users.noreply.github.com>
2020-05-16 15:05:11 +0800

    Add some parameters according to the documentation

--- py/database.sql
+++ py/database.sql
@@ -9,42 +9,46 @@ CREATE TABLE `DATABASE`.`users` (
 
 CREATE TABLE `DATABASE`.`selfCapsules` (
 	`capsule_id` INT NOT NULL AUTO_INCREMENT ,
-	`sender_id` INT NULL , --获取发送者的信息
+	`sender_id` INT NULL ,
 	`time_limit` INT NOT NULL ,
 	`cap_template` INT NOT NULL ,
 	`cap_location` INT NOT NULL ,
 	`content_word` TEXT NULL ,
 	`content_pic` TEXT NULL ,
 	`content_voice`  TEXT NULL,
- 	`registered` TIMESTAMP NOT NULL, --DEFAULT CURRENT_TIMESTAMP 新增胶囊形成时间
-	`sent` TIMESTAMP NOT NULL, --DEFAULT CURRENT_TIMESTAMP , --新增胶囊发送时间
 	PRIMARY KEY (`capsule_id`)
 ) ENGINE = InnoDB CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE TABLE `DATABASE`.`toTaCapsules` (
 	`capsule_id` INT NOT NULL AUTO_INCREMENT ,
-	`code` TEXT NULL ,   --取信码
-	`sender_name` INT NULL , --已注册使用注册的昵称，未注册使用微信用户名
-	`receiver_name` TEXT NOT NULL ,
-	`receiver_tel` TEXT NOT NULL ,
-	`receiver_email` TEXT NOT NULL ,
+	`code` INT NULL ,
 	`time_limit` INT NOT NULL ,
 	`cap_template` INT NOT NULL ,
+	`from_qrcode` BOOLEAN NOT NULL ,
 	`cap_location` INT NOT NULL ,
+    	`receiver_name` TEXT NOT NULL ,
+	`receiver_tel` TEXT NOT NULL ,
+	`receiver_email` TEXT NOT NULL ,
 	`content_word` TEXT NULL ,
 	`content_pic` TEXT NULL ,
-	`content_voice`  TEXT NULL,
- 	`registered` TIMESTAMP NOT NULL, --DEFAULT CURRENT_TIMESTAMP 新增胶囊形成时间
-	`sent` TIMESTAMP NOT NULL, --DEFAULT CURRENT_TIMESTAMP , --新增胶囊发送时间
+	`content_voice` TEXT NULL ,
 	`content_name` TEXT NULL DEFAULT NULL ,
 	`content_phone` INT NULL DEFAULT NULL ,
 	`content_birth` TIMESTAMP NULL DEFAULT NULL,
+	`xingzuo` TEXT NULL ,
+	`hobby` TEXT NULL ,
+	`music` TEXT NULL ,
+	`movie` TEXT NULL ,
+	`food`  TEXT NULL ,
+	`wechat` TEXT NULL ,
+	`QQ` TEXT NULL ,
+	`email` TEXT NULL ,
 	PRIMARY KEY (`capsule_id`)
 ) ENGINE = InnoDB CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE TABLE `DATABASE`.`strangerCapsules` (
 	`capsule_id` INT NOT NULL AUTO_INCREMENT ,
-	`sender_id` INT NULL , --获取发送者的信息
+	`sender_id` INT NULL ,
 	`time_limit` INT NOT NULL ,
 	`cap_template` INT NOT NULL ,
 	`cap_location` INT NOT NULL ,
