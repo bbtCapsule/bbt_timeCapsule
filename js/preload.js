7f085ddc4aca98819538ed6165d60e6fe80a61ba
Hikoyan <huanghongyan324@gmail.com>
2020-05-17 00:20:47 +0800

    add loading

--- js/preload.js
+++ js/preload.js
@@ -1,44 +1,39 @@
 $(function(){
+    allload();
     var nowpage=window.location.pathname.match(/(\w+.html)$/) [0];
     $("img").click(function(event){
         event.preventDefault();
         return false;
     })
-    function onepic(){
-        var backgroundownload = new createjs.LoadQueue(true);
-        backgroundownload.loadManifest([
-            "imgages/map.png",
-            "imgages/letter/1.png",
-            "imgages/letter/2.png",
-            "imgages/letter/3.png",
-            "imgages/letter/4.png",
-            "imgages/letter/41.png",
-        ]);
-        backgroundownload.load();
-    }
     function allload(){
         var imgdownload = new createjs.LoadQueue(true);
+        var idx=1;
+        var timer = setInterval(function(){
+            if(idx>3){
+                idx=1;
+            }
+            var src="./images/"+idx+".png";
+            idx++;
+            $("#rotate").attr("src",src);
+        },500)
         function handleComplete(){
-            //加载完成
+            //加载完成 等其他图片放进来以后 把图片放进↓的加载列表 就打开
+           // $("#loading").fadeOut(800);
+           //clearInterval(timer);
         }
         // imgdownload.on("fileload", handleFileLoad, this);
         imgdownload.on("complete", handleComplete, this);
         imgdownload.loadManifest([
-            // "css/index.css",
-            "imgages/map.png",
+            "./images/map.jpg",
+            "./images/letter/1.png",
+            "./images/letter/2.png",
+            "./images/letter/3.png",
+            "./images/letter/4.png",
+            "./images/letter/41.png",
         ]);
     imgdownload.load();
 }
-function loading(){
-    var imgdownload = new createjs.LoadQueue(true);
-    function handleComplete(){
-        console.log("completed");
-        complete();
-        anime(1);
-    }
-    imgdownload.on("complete", handleComplete, this);
-    imgdownload.loadManifest([
-]);
-imgdownload.load();
-}
+
+
+
 })
