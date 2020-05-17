06c4d8591e91d047cd967362e44ebbd633cd66ac
kimivictory <2382724968@qq.com>
2020-05-17 08:41:56 +0800

    页面跳转和初始页

--- js/write.js
+++ js/write.js
@@ -2,35 +2,35 @@
 var capsule_type, time_limit, cap_template, cap_location, content_word, content_pic, content_voice, content_name, content_phone, content_birth;
 //页面初始的显示
 var page = {
-    page1: document.getElementById('page1'),
-    page2: document.getElementById('page2'),
-    writeone: document.getElementById('write-one'),
-    writesec: document.getElementById('write-sec'),
-    writeTA: document.getElementById('write-TA'),
-    writeTAsend: document.getElementById('write-TA-send'),
-    writemap: document.getElementById('write-map'),
-    finish: document.getElementById('finish')
+    page1: $('#page1'),
+    page2: $('#page2'),
+    writeone: $('#write-one'),
+    writesec: $('#write-sec'),
+    writeTA: $('#write-TA'),
+    writeTAsend: $('#write-TA-send'),
+    writemap: $('#write-map'),
+    finish: $('#finish')
 }
 for (var key in page) {
-    page[key].setAttribute('style', 'display:none;');
+    page[key].attr('style', 'display:none;');
 }
 window.onload = function () {
-    page.page1.setAttribute('style', 'display:block;');
+    page.page1.attr('style', 'display:block;');
 }
 var nextPage = {
-    page1: $('.nextPage')[0],
-    page2: $('.nextPage')[1],
-    writeone: $('.nextPage')[2],
-    writesec: $('.nextPage')[3],
-    writeTA: $('.nextPage')[4],
-    writeTAsend: $('.nextPage')[5]
+    page1: $('#page1 .nextPage'),
+    page2: $('#page2 .nextPage'),
+    writeone: $('#write-one .nextPage'),
+    writesec: $('#write-sec .nextPage'),
+    writeTA: $('#write-TA .nextPage'),
+    writeTAsend: $('#write-TA-send .nextPage')
 }
 var prePage = {
-    page1: $('.prePage')[0],
-    page2: $('.prePage')[1],
-    writeone: $('.prePage')[2],
-    writesec: $('.prePage')[3],
-    writeTA: $('.prePage')[4]
+    page1: $('#page1 .prePage'),
+    page2: $('#page2 .prePage'),
+    writeone: $('#write-one .prePage'),
+    writesec: $('#write-sec .prePage'),
+    writeTA: $('#write-TA .prePage')
 }
 
 //函数
@@ -47,40 +47,40 @@ function deleteSpace(str) {
     return str.replace(/\s/g, '');
 }
 //页面开关
-prePage.page1.addEventListener('click', function () {
+prePage.page1.on('click', function () {
     window.location.href = 'main.html';
 })
 //1&2 开始
-nextPage.page1.addEventListener('click', function () {
+nextPage.page1.on('click', function () {
     capsule_type = getRadio(document.getElementsByName('sender'));
     if (capsule_type === null) {
         alert('无信息');
     } else {
-        page.page1.setAttribute('style', 'display:none;');
-        page.page2.setAttribute('style', 'display:block;')
+        page.page1.attr('style', 'display:none;');
+        page.page2.attr('style', 'display:block;')
     }
 })
-prePage.page2.addEventListener('click', function () {
-    page.page2.setAttribute('style', 'display:none;');
-    page.page1.setAttribute('style', 'display:block;');
+prePage.page2.on('click', function () {
+    page.page2.attr('style', 'display:none;');
+    page.page1.attr('style', 'display:block;');
 })
 //1&2 结束
 //page2的分支有三个 
 //type 若0 选了信纸1、2。 若1选了信纸3。 若2选了同学录
 function switchPage(type) {
     if (type == 0) {
-        page.page2.setAttribute('style', 'display:none;');
-        page.writeone.setAttribute('style', 'display:block;');
+        page.page2.attr('style', 'display:none;');
+        page.writeone.attr('style', 'display:block;');
     } else if (type == 1) {
-        page.page2.setAttribute('style', 'display:none;');
-        page.writesec.setAttribute('style', 'display:block;');
+        page.page2.attr('style', 'display:none;');
+        page.writesec.attr('style', 'display:block;');
     } else if (type == 2) {
-        page.page2.setAttribute('style', 'display:none;');
-        page.writeTA.setAttribute('style', 'display:block;');
+        page.page2.attr('style', 'display:none;');
+        page.writeTA.attr('style', 'display:block;');
     }
 }
 //2&writeone&writesec&writeTA
-nextPage.page2.addEventListener('click', function () {
+nextPage.page2.on('click', function () {
     time_limit = getRadio(document.getElementsByName('year'));
     var template = getRadio(document.getElementsByName('template'));
     if (time_limit === null) {
@@ -99,51 +99,58 @@ nextPage.page2.addEventListener('click', function () {
     }
 
 })
-prePage.writeone.addEventListener('click', function () {
-    page.writeone.setAttribute('style', 'display:none;');
-    page.page2.setAttribute('style', 'display:block;');
+prePage.writeone.on('click', function () {
+    page.writeone.attr('style', 'display:none;');
+    page.page2.attr('style', 'display:block;');
 })
-prePage.writesec.addEventListener('click', function () {
-    page.writesec.setAttribute('style', 'display:none;');
-    page.page2.setAttribute('style', 'display:block;');
+prePage.writesec.on('click', function () {
+    page.writesec.attr('style', 'display:none;');
+    page.page2.attr('style', 'display:block;');
 })
-prePage.writeTA.addEventListener('click', function () {
-    page.writeTA.setAttribute('style', 'display:none;');
-    page.page2.setAttribute('style', 'display:block;');
+prePage.writeTA.on('click', function () {
+    page.writeTA.attr('style', 'display:none;');
+    page.page2.attr('style', 'display:block;');
 })
-//writeone&writemap
-nextPage.writeone.addEventListener('click', function () {
+//writeone&writemap 1\2信纸
+nextPage.writeone.on('click', function () {
     // cap_template, content_word
     content_word = $('.letter_text').val();
     content_word = deleteSpace(content_word);
     if (content_word == '') {
         alert('无信息');
     } else {
-        page.writeone.setAttribute('style', 'display:none;');
-        page.writemap.setAttribute('style', 'display:block;')
+        page.writeone.attr('style', 'display:none;');
+        page.writemap.attr('style', 'display:block;')
     }
 })
 // writeone&writemap 结束
 //writesec&writemap 开始
-nextPage.writesec.addEventListener('click', function () {
-    page.writesec.setAttribute('style', 'display:none;');
-    page.writemap.setAttribute('style', 'display:block;')
+nextPage.writesec.on('click', function () {
+    // cap_template, content_word
+    content_word = $('.letter_text').val();
+    content_word = deleteSpace(content_word);
+    if (content_word == '') {
+        alert('无信息');
+    } else {
+        page.writesec.attr('style', 'display:none;');
+        page.writemap.attr('style', 'display:block;')
+    }
 })
 //writeTA&writeTAsend 开始
-nextPage.writeTA.addEventListener('click', function () {
+nextPage.writeTA.on('click', function () {
     // cap_template, content_word
     content_word = $('#content_word_TA').val();
     content_word = deleteSpace(content_word);
     if (content_word == '') {
         alert('无信息');
     } else {
-        page.writeTA.setAttribute('style', 'display:none;');
-        page.writeTAsend.setAttribute('style', 'display:block;')
+        page.writeTA.attr('style', 'display:none;');
+        page.writeTAsend.attr('style', 'display:block;')
     }
 })
 //writeTA&writeTAsend 结束
 // writeTAsend&writemap 开始
-nextPage.writeTAsend.addEventListener('click', function () {
+nextPage.writeTAsend.on('click', function () {
     // content_name,content_phone,content_birth;
     content_name = $('#content_name').val();
     content_phone = $('#content_phone').val();
@@ -160,18 +167,20 @@ nextPage.writeTAsend.addEventListener('click', function () {
     if (sum === false) {
         alert('无信息或信息错误');
     } else {
-        page.writeTAsend.setAttribute('style', 'display:none;');
-        page.writemap.setAttribute('style', 'display:block;')
+        page.writeTAsend.attr('style', 'display:none;');
+        page.writemap.attr('style', 'display:block;')
     }
 })
 // writeTAsend&writemap 结束
 $('#images').on('click', function () {
     chooseImg(0);
 });
-$('#voiceStart').on('touchstart', function () {
+$('#voiceStart').on('touchstart', function (event) {
+    event.preventDefault();
     voiceRecord(0, 3000);
 });
 $('#voiceStart').on('touchend', function () {
+    event.preventDefault();
     voiceRecord(1, 3000);
 });
 $('#voicePlay').on('click', function () {
