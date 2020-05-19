0d8f549a99b3c821e997c600c71085324160dc8b
zekaio <zekailing@gmail.com>
2020-05-20 04:02:08 +0800

    收信二维码测试完毕

---
+++ js/receive.js
@@ -0,0 +1,20 @@
+fetch(apiurl + "getQRCode", {
+  method: "get",
+  credentials: "include",
+  mode: "cors",
+})
+  .then(checkStatus)
+  .then((res) => res.json())
+  .then((res) => {
+    if (res.errcode == 0) {
+      document.getElementById("QR").src = res.image;
+    }
+  })
+  .catch((err) => {
+    if (err.res.status == 404) {
+      attention("请先填写个人信息");
+      setTimeout(() => {
+        window.location.href = "main.html";
+      }, 1000);
+    }
+  });
